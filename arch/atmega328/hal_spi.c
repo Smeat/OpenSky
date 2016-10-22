7f87bdea16b52f50d4c8de48fb8331a9485e2986
Jimmy Wennlund <jimmy@wennlund.nu>
2016-10-22 22:58:54 +0200

    Telemetry seems to not work if spi is slower then OSC/4

--- arch/atmega328/hal_spi.c
+++ arch/atmega328/hal_spi.c
@@ -19,15 +19,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-ISR(SPI_STC_vect){
-}
+/*  SPR0
+ *  SPI2X    SPR01   SPR00   DESCRIPTION
+ *  0        0       0       Clock / 4
+ *  0        0       1       Clock / 16
+ *  0        1       0       Clock / 64
+ *  0        1       1       Clock / 128
+ *  1        0       0       Clock / 2
+ *  1        0       0       Clock / 8
+ *  1        0       1       Clock / 32
+ *  1        1       1       Clock / 64
+ */
+
+/* Note, by some reason telemetry fails if divider is higher the 4 */
 
 void hal_spi_init(void) {
-  /* Enable SPI, Master, set clock rate fck/16 */
-  SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+  /* Enable SPI, Master, set clock rate fck/4 */
+  SPCR = (1<<SPE)|(1<<MSTR)|(0<<SPI2X)|(0<<SPR1)|(0<<SPR0);
 }
 
-uint8_t hal_spi_tx(uint8_t address) {
+uint8_t inline hal_spi_tx(uint8_t address) {
 
   /* Send byte */
   SPDR = address;
@@ -36,7 +47,7 @@ uint8_t hal_spi_tx(uint8_t address) {
   while(!(SPSR & (1<<SPIF))) {}
 
   /* Return byte */
-  uint8_t r = SPDR;
+  const uint8_t r = SPDR;
   //debug("hal_spi_tx sent: 0x"); debug_put_hex8(address); debug(" got: 0x"); debug_put_hex8(r); debug("\n"); debug_flush();
   return r;
 }
